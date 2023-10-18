54757616542fcf01ceb5b790bbaeedaace388c70
Beatriz Franco <beatriz.dfranco@gmail.com>
2023-10-17 22:44:38 -0300

    My first javascript commit

---
+++ myscript.js
@@ -0,0 +1,14 @@
+// Insira no rodapé das três páginas que você desenvolveu na Unidade 2 
+// (homepage, mais uma página e a página de formulário): nome completo, ano corrente, 
+// seus endereços das redes sociais, como: twitter, facebook, instagram, LinkedIn, sexo e 
+// localização de seu polo. Cada informação deve constar em linhas separadas usando 
+// VARIÁVEIS para o armazenamento das informações pessoais e exibindo-as com o 
+// document.write(), por meio da linguagem de script: JavaScript.
+
+var name = document.write("<p>Beatriz Franco</p>");
+var ano_corrente = document.write("<p>2023</p>");
+var twitter = document.write("<p>Twitter: @beatrizfranco</p>");
+var instagram = document.write("<p>Instagram: @beatrizfranco</p>");
+var linkedin = document.write("<p>Linkedin: @beatrizfranco</p>");
+var sexo = document.write("<p>Sexo: Feminino</p>")
+var localizacao_polo = document.write("<p>Polo: Taubaté</p>")
